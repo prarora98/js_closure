4ae9e4380b8311b29118672e82210f6fb1c9cdd9
prarora98 <prateekisthebest98@gmail.com>
2019-07-24 10:18:29 +0530

    closure done

--- closure/closure.js
+++ closure/closure.js
@@ -5,8 +5,43 @@ function nonsense(string) {
 }
 //2
 function nonsense(string) {
-  var blab = function () {
+  var blab = function() {
     alert(string);
   };
   setTimeout(blab, 2000);
+}
+//3
+function nonsense(string) {
+  return function blab() {
+    alert(string);
+  };
+}
+var blabLater = nonsense("hello");
+var blabAgainLater = nonsense("hel");
+
+//4
+var lastNameTrier = function(firstName) {
+  //does stuff
+
+  var innerFunction = function(lastName) {
+    console.log(`${firstName} ${lastName}`);
+  };
+  return innerFunction;
+};
+var firstNameFarmer = lastNameTrier("Farmer"); //logs nothing
+firstNameFarmer("Brown"); //logs 'Farmer Brown'
+
+//5
+function storyWriter() {
+  let story = "";
+  return {
+    addWords: str => {
+      story = story + " " + str;
+      console.log(story);
+    },
+    erase: () => {
+      story = "";
+      console.log(story);
+    }
+  };
 }
